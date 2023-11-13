513b1e12320d1dcb3b575cf62bcbc2e4154bd686
LUCASDALLACOSTA <lucas.dallacosta@limayrac.fr>
2023-11-13 14:41:00 +0100

    initialisation

---
+++ declarationfrais/src/main/java/fr/projet/declarationfrais/DeclarationfraisApplication.java
@@ -0,0 +1,13 @@
+package fr.projet.declarationfrais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeclarationfraisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeclarationfraisApplication.class, args);
+	}
+
+}
---
+++ declarationfrais/src/test/java/fr/projet/declarationfrais/DeclarationfraisApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projet.declarationfrais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeclarationfraisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
