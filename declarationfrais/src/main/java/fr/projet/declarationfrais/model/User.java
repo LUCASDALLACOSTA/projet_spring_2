d2d929f9f718b14acd414a34691f16d19ac3f2f4
LUCASDALLACOSTA <lucas.dallacosta@limayrac.fr>
2023-11-20 16:02:09 +0100

    declaration-flows

--- declarationfrais/src/main/java/fr/projet/declarationfrais/model/User.java
+++ declarationfrais/src/main/java/fr/projet/declarationfrais/model/User.java
@@ -6,17 +6,23 @@ import javax.persistence.*;
 @Data
 @Entity
 @Table(name = "user")
-
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, unique = true, length = 50)
     private String email;
+
     @Column(nullable = false, length = 100)
     private String password;
+
     @Column(name = "first_name", nullable = false, length = 20)
     private String firstName;
+
     @Column(name = "last_name", nullable = false, length = 20)
-    private String LastName;
+    private String lastName;
+
+    @Column(nullable = false)
+    private boolean admin;
 }
