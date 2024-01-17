257cc63027cc36a98e34fb92554d257f40fc3c0e
LUCASDALLACOSTA <lucas.dallacosta@limayrac.fr>
2024-01-17 13:57:33 +0100

    modif statut

--- declarationfrais/src/main/java/fr/projet/declarationfrais/services/DeclarationService.java
+++ declarationfrais/src/main/java/fr/projet/declarationfrais/services/DeclarationService.java
@@ -23,44 +23,6 @@ public class DeclarationService {
         this.declarationRepository = declarationRepository;
     }
 
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
-
-    public Declaration sauvegarderDeclaration(RequestContext RequestContext) {
-        String refDossier = (String) RequestContext.getFlowScope().get("refDossier");
-        Date date = (Date) RequestContext.getFlowScope().get("date");
-        String lieu = (String) RequestContext.getFlowScope().get("lieu");
-        String intitule = (String) RequestContext.getFlowScope().get("intitule");
-        String type_transport = (String) RequestContext.getFlowScope().get("type_transport");
-        String lieu_depart = (String) RequestContext.getFlowScope().get("lieu_depart");
-        String facture_transport = (String) RequestContext.getFlowScope().get("facture_transport");
-        String montant_transport = (String) RequestContext.getFlowScope().get("montant_transport");
-        String type_hebergement = (String) RequestContext.getFlowScope().get("type_hebergement");
-        String facture_hebergement = (String) RequestContext.getFlowScope().get("facture_hebergement");
-        String montant_hebergement = (String) RequestContext.getFlowScope().get("montant_hebergement");
-        String coordonneesbancaires = (String) RequestContext.getFlowScope().get("coordonneesbancaires");
-
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(connectedUserEmail);
-
-        Declaration declaration = new Declaration();
-        declaration.setUser(connectedUserId);
-        declaration.setStatut("en attente");
-        declaration.setRefDossier(refDossier);
-        declaration.setDate(date);
-        declaration.setLieu(lieu);
-        declaration.setIntitule(intitule);
-        declaration.setTypeTransport(type_transport);
-        declaration.setLieuDepart(lieu_depart);
-        declaration.setNom_fichier_transport(facture_transport);
-        declaration.setMontant_transport(montant_transport);
-        declaration.setType_hebergement(type_hebergement);
-        declaration.setNom_fichier_hebergement(facture_hebergement);
-        declaration.setMontant_hebergement(montant_hebergement);
-        declaration.setCoordonneesbancaires(coordonneesbancaires);
-
-        return DeclarationRepository.save(declaration);
-    }
-
     public List<Declaration> getAllDeclarations() {
         return declarationRepository.findAll();
     }
